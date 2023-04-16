7196ed735715200c1b56c31fe8ccc5b6da5d8db7
Minh Pham <mp5847@log-1.hpc.nyu.edu>
2023-04-16 14:36:11 -0400

    refactor precompute_output.py

--- precompute_output.sh
+++ precompute_output.sh
@@ -1,18 +1,9 @@
 cd /home/mp5847/src/wise-ft; export PYTHONPATH="$PYTHONPATH:$PWD"; \
 
-torchrun src/precompute_output.py  --num_epoch 400 \
-                                    --learning_rate 0.01 \
+python3 src/precompute_output.py --model "coca_ViT-L-14" \
+                                    --pretrained "laion2B-s13B-b90k" \
+                                    --teacher-ckpt "/scratch/mp5847/wise-ft-ckpt/coca_ViT-L-14-pretrained=laion2B-s13B-b90k/finetuned/wise_ft_alpha=0.000.pt" \
+                                    --batch_size 512 \
                                     --cuda \
                                     --workers 20 \
-                                    --model RN50 \
-                                    --pretrained "" \
-                                    --teacher-ckpt "/scratch/mp5847/wise-ft-kd/rn50_scratch_1/model_99.pt" \
-                                    --batch_size 512 \
-                                    --dist-url "env://" \
-                                    --dist-backend "nccl" \
-                                    --multiprocessing-distributed \
-                                    --world-size 1 \
-                                    --template openai_imagenet_template \
-                                    --train-dataset=ImageNet \
-                                    --data-location=/ \
-                                    --rank 0
+                                    --data-location=/ 
--- src/precompute_output.py
+++ src/precompute_output.py
@@ -55,13 +55,9 @@ def build_parser():
     parser.add_argument("--learning_rate", type=float, default=0.01, help="learning rate for models")
     parser.add_argument("--num_epoch", type=int, default=100, help="number of epochs")
     parser.add_argument("--cuda", action='store_true', help="use gpu or not")
-    parser.add_argument("--name", type=str, default="test", help="name for experiment to save")
-    parser.add_argument("--num_step", type=int, default=40)
-    parser.add_argument("--log_interval", type=int, default=10)
-    parser.add_argument("--optimizer", type=str, default="SGD")
 
     #student model
-    parser.add_argument("--model", type=str, default="RN50")  
+    parser.add_argument("--model", type=str, default="RN50")
     parser.add_argument("--pretrained", type=str, default="")  
 
     #teacher model
@@ -71,46 +67,6 @@ def build_parser():
     
     parser.add_argument('-j', '--workers', default=4, type=int, metavar='N',
                         help='number of data loading workers (default: 4)')
-    parser.add_argument('--epochs', default=90, type=int, metavar='N',
-                        help='number of total epochs to run')
-    parser.add_argument('--start-epoch', default=0, type=int, metavar='N',
-                        help='manual epoch number (useful on restarts)')
-    parser.add_argument('-b', '--batch-size', default=32, type=int,
-                        metavar='N',
-                        help='mini-batch size (default: 256), this is the total '
-                            'batch size of all GPUs on the current node when '
-                            'using Data Parallel or Distributed Data Parallel')
-    parser.add_argument('--lr', '--learning-rate', default=0.1, type=float,
-                        metavar='LR', help='initial learning rate', dest='lr')
-    parser.add_argument('--momentum', default=0.9, type=float, metavar='M',
-                        help='momentum')
-    parser.add_argument('--wd', '--weight-decay', default=1e-4, type=float,
-                        metavar='W', help='weight decay (default: 1e-4)',
-                        dest='weight_decay')
-    parser.add_argument('-p', '--print-freq', default=10, type=int,
-                        metavar='N', help='print frequency (default: 10)')
-    parser.add_argument('--resume', default='', type=str, metavar='PATH',
-                        help='path to latest checkpoint (default: none)')
-    parser.add_argument('-e', '--evaluate', dest='evaluate', action='store_true',
-                        help='evaluate model on validation set')
-
-    parser.add_argument('--world-size', default=-1, type=int,
-                        help='number of nodes for distributed training')
-    parser.add_argument('--rank', default=-1, type=int,
-                        help='node rank for distributed training')
-    parser.add_argument('--dist-url', default='tcp://224.66.41.62:23456', type=str,
-                        help='url used to set up distributed training')
-    parser.add_argument('--dist-backend', default='nccl', type=str,
-                        help='distributed backend')
-    parser.add_argument('--seed', default=None, type=int,
-                        help='seed for initializing training. ')
-    parser.add_argument('--gpu', default=None, type=int,
-                        help='GPU id to use.')
-    parser.add_argument('--multiprocessing-distributed', action='store_true',
-                        help='Use multi-processing distributed training to launch '
-                            'N processes per node, which has N GPUs. This is the '
-                            'fastest way to use PyTorch for either single node or '
-                            'multi node data parallel training')
     
     #wise-ft
     parser.add_argument(
@@ -162,12 +118,7 @@ def main(args):
     # create student model
     image_encoder_student_model, image_encoder_student_train_preprocess, image_encoder_student_val_preprocess = open_clip.create_model_and_transforms(
             args.model, pretrained=args.pretrained, device=device, jit=False)
-    # image_encoder_student = ImageEncoder(args, keep_lang=True)
-    # image_encoder_student.model = image_encoder_student_model
-    # image_encoder_student.train_preprocess = image_encoder_student_train_preprocess
-    # image_encoder_student.val_preprocess = image_encoder_student_val_preprocess
-    # classification_head_student = get_zeroshot_classifier(args, image_encoder_student_model)
-    # model_student =  ImageClassifier(image_encoder_student, classification_head_student, process_images=True)
+
 
     # create teacher model
     model_teacher = ImageClassifier.load(args.teacher_ckpt)
